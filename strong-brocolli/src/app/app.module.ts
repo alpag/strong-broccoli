afb38012bf1e6275f231368a5a3f4e85cf7cddda
alpag <pawlaq123@gmail.com>
2019-02-12 12:36:46 +0100

    Init angular

---
+++ strong-brocolli/src/app/app.module.ts
@@ -0,0 +1,23 @@
+//DEFAULT
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+//MATERIAL
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
